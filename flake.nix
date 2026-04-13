61ba2dbfe1052b62c2529a3c32a24667dd801cc6
Myxogastria0808 <r.rstudio.c@gmail.com>
2026-04-13 19:40:41 +0900

    feat: add builer nix description

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-        toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        toolchain = pkgs.rust-bin.stable.latest.default;
         rustPlatform = pkgs.makeRustPlatform {
           rustc = toolchain;
           cargo = toolchain;
@@ -37,6 +37,35 @@
               (rust-bin.stable.latest.default.override { extensions = [ "rust-src" ]; })
             ];
             RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+          };
+        packages =
+          let
+            biwac = rustPlatform.buildRustPackage {
+              pname = "biwac";
+              version = "0.1.0";
+              src = ./.;
+              cargoLock.lockFile = ./Cargo.lock;
+              doCheck = false;
+            };
+          in
+          {
+            sample = pkgs.stdenv.mkDerivation {
+              name = "biwa-example";
+              src = ./assets/tests/test1;
+
+              nativeBuildInputs = [ biwac ];
+
+              buildPhase = ''
+                mkdir -p .biwa_build
+                cp biwa-dependencies.json .biwa_build/
+                biwac
+              '';
+
+              installPhase = ''
+                mkdir -p $out/.biwa_build/
+                cp .biwa_build/typescript/src/generated/*.ts $out/.biwa_build/
+              '';
+            };
           };
       }
     );
