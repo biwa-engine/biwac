d53e6a87ec3fd881466d59b255beb720075c79ef
reversed-R <qiu1287shan@gmail.com>
2026-04-13 20:41:59 +0900

    fix: separate parser error mod

---
+++ src/biwac_parser/src/error.rs
@@ -0,0 +1,103 @@
+// use std::fmt::Display;
+
+use biwac_base::BiwacError;
+use biwac_lexer::{TkKind, Token};
+use biwac_novel_parser::NovelParseError;
+
+#[derive(Debug, Clone)]
+pub enum ParseError {
+    InvalidToken(Vec<TkKind>, Token), // expected TokenKind, ... or TokenKind, but found Token in Token.range
+    InvalidEOF(Vec<TkKind>),          // expected TokenKind, ... or TokenKind, but found EOF
+    StructMemberConflict(String, String, Box<Token>), // struct id, member id
+    NovelParseError(NovelParseError),
+}
+
+impl BiwacError for ParseError {
+    fn error_message(&self) -> String {
+        todo!()
+    }
+}
+
+// impl Display for TkKind {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         match self {
+//             Self::Ident(s) => {
+//                 if s.is_empty() {
+//                     write!(f, "identifier")
+//                 } else {
+//                     write!(f, "identifier `{s}`")
+//                 }
+//             }
+//             Self::IntLiteral(i) => write!(f, "literal `{i}`"),
+//             Self::CharLiteral(c) => write!(f, "literal `'{c}'`"),
+//             Self::StringLiteral(s) => write!(f, "literal `\"{s}\"`"),
+//             _ => write!(f, "`{}`", self.pattern()),
+//         }
+//     }
+// }
+
+//
+// impl ParseError {
+//     pub fn panic_with_error_message(&self, src: &str) -> ! {
+//         eprint!("\x1b[1;38;2;255;20;0merror\x1b[m: ");
+//         match self {
+//             Self::InvalidToken(expecteds, found) => {
+//                 eprintln!(
+//                     "\x1b[1mexpected {}, but found {} in the {}th character\x1b[m",
+//                     string_of(expecteds),
+//                     found.kind,
+//                     found.range.begin
+//                 );
+//                 eprintln!(
+//                     "... {}\x1b[4m{}\x1b[m{} ...",
+//                     &src[0.max(found.range.begin - 10)..found.range.begin],
+//                     &src[found.range.begin..found.range.end],
+//                     &src[found.range.end..src.len().min(found.range.end + 10)],
+//                 );
+//             }
+//             Self::InvalidEOF(expecteds) => {
+//                 eprintln!(
+//                     "\x1b[1mexpected {}, but found EOF\x1b[m",
+//                     string_of(expecteds),
+//                 );
+//                 eprintln!(
+//                     "... {}\x1b[4m{}\x1b[m",
+//                     &src[0.max(src.len() - 20)..0.max(src.len() - 5)],
+//                     &src[0.max(src.len() - 5)..],
+//                 );
+//             }
+//             Self::StructMemberConflict(structid, memberid, range) => {
+//                 eprintln!("\x1b[1mmember `{memberid}` in struct `{structid}` is conflicted\x1b[m",);
+//                 eprintln!(
+//                     "... {}\x1b[4m{}\x1b[m{} ...",
+//                     &src[0.max(range.begin - 10)..range.begin],
+//                     &src[range.begin..range.end],
+//                     &src[range.end..src.len().min(range.end + 10)],
+//                 );
+//             }
+//         }
+//
+//         panic!("");
+//     }
+// }
+//
+//
+// fn string_of(kinds: &[TokenKind]) -> String {
+//     if kinds.is_empty() {
+//         "".to_string()
+//     } else if kinds.len() == 1 {
+//         kinds.first().unwrap().to_string()
+//     } else if kinds.len() == 2 {
+//         format!("{} or {}", kinds.first().unwrap(), kinds.last().unwrap())
+//     } else {
+//         format!(
+//             "{}{} or {}",
+//             &kinds[..kinds.len() - 2]
+//                 .iter()
+//                 .map(|kind| format!("{kind}, "))
+//                 .collect::<String>(),
+//             &kinds[kinds.len() - 2..].first().unwrap(),
+//             &kinds[kinds.len() - 2..].last().unwrap()
+//         )
+//     }
+// }
--- src/biwac_parser/src/lib.rs
+++ src/biwac_parser/src/lib.rs
@@ -1,3 +1,4 @@
+mod error;
 pub mod macros;
 pub mod symbols;
 pub mod types;
@@ -9,16 +10,9 @@ use biwac_ast::{Ident, QualifiedId};
 use biwac_base::Span;
 use biwac_lexer::{TkKind, Token};
 
-use biwac_novel_parser::NovelParseError;
 pub(crate) use symbols::statements::ExprOrStmt;
 
-#[derive(Debug, Clone)]
-pub enum ParseError {
-    InvalidToken(Vec<TkKind>, Token), // expected TokenKind, ... or TokenKind, but found Token in Token.range
-    InvalidEOF(Vec<TkKind>),          // expected TokenKind, ... or TokenKind, but found EOF
-    StructMemberConflict(String, String, Box<Token>), // struct id, member id
-    NovelParseError(NovelParseError),
-}
+pub use error::ParseError;
 
 #[derive(Debug, Clone)]
 pub struct Parser {
@@ -157,86 +151,3 @@ impl<'t> TokenStream<'t> {
         }
     }
 }
-
-//
-// impl ParseError {
-//     pub fn panic_with_error_message(&self, src: &str) -> ! {
-//         eprint!("\x1b[1;38;2;255;20;0merror\x1b[m: ");
-//         match self {
-//             Self::InvalidToken(expecteds, found) => {
-//                 eprintln!(
-//                     "\x1b[1mexpected {}, but found {} in the {}th character\x1b[m",
-//                     string_of(expecteds),
-//                     found.kind,
-//                     found.range.begin
-//                 );
-//                 eprintln!(
-//                     "... {}\x1b[4m{}\x1b[m{} ...",
-//                     &src[0.max(found.range.begin - 10)..found.range.begin],
-//                     &src[found.range.begin..found.range.end],
-//                     &src[found.range.end..src.len().min(found.range.end + 10)],
-//                 );
-//             }
-//             Self::InvalidEOF(expecteds) => {
-//                 eprintln!(
-//                     "\x1b[1mexpected {}, but found EOF\x1b[m",
-//                     string_of(expecteds),
-//                 );
-//                 eprintln!(
-//                     "... {}\x1b[4m{}\x1b[m",
-//                     &src[0.max(src.len() - 20)..0.max(src.len() - 5)],
-//                     &src[0.max(src.len() - 5)..],
-//                 );
-//             }
-//             Self::StructMemberConflict(structid, memberid, range) => {
-//                 eprintln!("\x1b[1mmember `{memberid}` in struct `{structid}` is conflicted\x1b[m",);
-//                 eprintln!(
-//                     "... {}\x1b[4m{}\x1b[m{} ...",
-//                     &src[0.max(range.begin - 10)..range.begin],
-//                     &src[range.begin..range.end],
-//                     &src[range.end..src.len().min(range.end + 10)],
-//                 );
-//             }
-//         }
-//
-//         panic!("");
-//     }
-// }
-//
-// impl Display for TokenKind {
-//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-//         match self {
-//             Self::Identifier(s) => {
-//                 if s.is_empty() {
-//                     write!(f, "identifier")
-//                 } else {
-//                     write!(f, "identifier `{s}`")
-//                 }
-//             }
-//             Self::IntLiteral(i) => write!(f, "literal `{i}`"),
-//             Self::CharLiteral(c) => write!(f, "literal `'{c}'`"),
-//             Self::StringLiteral(s) => write!(f, "literal `\"{s}\"`"),
-//             _ => write!(f, "`{}`", self.pattern()),
-//         }
-//     }
-// }
-//
-// fn string_of(kinds: &[TokenKind]) -> String {
-//     if kinds.is_empty() {
-//         "".to_string()
-//     } else if kinds.len() == 1 {
-//         kinds.first().unwrap().to_string()
-//     } else if kinds.len() == 2 {
-//         format!("{} or {}", kinds.first().unwrap(), kinds.last().unwrap())
-//     } else {
-//         format!(
-//             "{}{} or {}",
-//             &kinds[..kinds.len() - 2]
-//                 .iter()
-//                 .map(|kind| format!("{kind}, "))
-//                 .collect::<String>(),
-//             &kinds[kinds.len() - 2..].first().unwrap(),
-//             &kinds[kinds.len() - 2..].last().unwrap()
-//         )
-//     }
-// }
