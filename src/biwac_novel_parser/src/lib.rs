977659837e6ec679b549506d16393e575af94497
reversed-R <qiu1287shan@gmail.com>
2026-08-25 21:48:29 +0900

    fix: novel parser char byte index

--- src/biwac_novel_parser/src/lib.rs
+++ src/biwac_novel_parser/src/lib.rs
@@ -138,7 +138,10 @@ impl<'src> NovelSourceStream<'src> {
         // NOTE:
         // コマンド行であれば
         // カーソル位置をその次にずらす
-        match self.src.chars().nth(self.line_begin_idx + tmp_idx_in_line) {
+        //
+        // 各 idx はバイトインデックスなので、文字数を取る chars().nth() には渡せない
+        // (マルチバイト文字を含む行で位置がずれる)
+        match self.src[self.line_begin_idx + tmp_idx_in_line..].chars().next() {
             Some('#') => {
                 self.idx = self.line_begin_idx + tmp_idx_in_line + 1;
                 Some(NovelLineKind::GeneralCommand)
